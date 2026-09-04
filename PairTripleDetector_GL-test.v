18c4b51619a0136b234cfdd5f250784864ff13bb
bc678 <bc678@cornell.edu>
2026-09-04 15:28:12 -0400

    finished implementation

--- PairTripleDetector_GL-test.v
+++ PairTripleDetector_GL-test.v
@@ -82,6 +82,7 @@ module Top();
   task test_case_2_exhaustive();
     t.test_case_begin( "test_case_2_exhaustive" );
 
+    // in0 in1 in2 out
     check( 0,  0,  0,  0 );
     check( 1,  0,  0,  0 );
     check( 0,  1,  0,  0 );
